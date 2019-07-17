54faabf1c004cf6a809e28b73e1a13ccdb4d91d6
Ivan <Kishkan91@gmail.com>
2019-07-18 01:37:08 +0400

    task3 init

---
+++ src/task3/TextSorterMain.java
@@ -0,0 +1,10 @@
+package task3;
+
+//Отсортировать все слова из текста двумя способами: естественной сортировкой(по алфавиту)
+//и по длине слов (Подсказка: лучше и проще использовать Comparator).
+
+public class TextSorterMain {
+    public static void main(String[] args) {
+
+    }
+}
