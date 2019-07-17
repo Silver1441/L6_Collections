4f934bad799a24f9598412909515f65b03344016
Ivan <Kishkan91@gmail.com>
2019-07-18 01:34:04 +0400

    task1 stream api is used.

--- src/task1/service/DemoOutput.java
+++ src/task1/service/DemoOutput.java
@@ -4,9 +4,10 @@ import java.util.Map;
 
 public class DemoOutput {
     public static void showOutput(Map<String, Long> words){
-        for (Map.Entry<String, Long> entry : words.entrySet()) {
-            System.out.println("\"" + entry.getKey() + "\"" + "  -  times in text: " + entry.getValue());
-        }
+        words.entrySet().stream()
+                .forEach(word -> System.out.println("\"" + word.getKey() + "\"" +
+                        "  - times in text: " + word.getValue()));
+
         System.out.println();
         System.out.println("Total unique words count:  " + words.size());
     }
