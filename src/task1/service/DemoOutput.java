9a072ff8759577864284d91cae53dfe168f87966
Ivan <Kishkan91@gmail.com>
2019-07-19 23:39:21 +0400

    MatcherGroupIterator is added

--- src/task1/service/DemoOutput.java
+++ src/task1/service/DemoOutput.java
@@ -3,7 +3,7 @@ package task1.service;
 import java.util.Map;
 
 public class DemoOutput {
-    public static void showOutput(Map<String, Long> words){
+    public static void showOutput(Map<String, Long> words) {
         words.entrySet().stream()
                 .forEach(word -> System.out.println("\"" + word.getKey() + "\"" +
                         "  - times in text: " + word.getValue()));
--- src/task2/service/UniqueWordsCounter.java
+++ src/task2/service/UniqueWordsCounter.java
@@ -1,24 +1,57 @@
 package task2.service;
 
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
+import static java.util.stream.Collectors.toSet;
 import static template.Patterns.*;
 
 public class UniqueWordsCounter {
     public static Set<String> countUniqueWords(String text) {
-        Set<String> words = new HashSet<>();
-
         Pattern pattern = Pattern.compile(ONLY_WORDS);
-        Matcher matcher = pattern.matcher(text);
 
-        while (matcher.find()) {
-            words.add(text.substring(matcher.start(), matcher.end()));
+        Set<String> result = (StreamSupport.stream(new MatcherGroupIterator(pattern,text),false))
+                //.map( s -> s.toLowerCase() )
+                .collect(Collectors.toSet());
+        return result;
+
+    }
+
+    private static class MatcherGroupIterator implements Spliterator<String> {
+        private final Matcher matcher;
+
+        public MatcherGroupIterator(Pattern p, String s) {
+            matcher = p.matcher(s);
         }
 
-        return words;
+        @Override
+        public boolean tryAdvance(Consumer<? super String> action) {
+            if (!matcher.find()) {
+                return false;
+            }
+            action.accept(matcher.group());
+            return true;
+        }
+
+        @Override
+        public Spliterator<String> trySplit() {
+            return null;
+        }
+
+        @Override
+        public long estimateSize() {
+            return Long.MAX_VALUE;
+        }
+
+        @Override
+        public int characteristics() {
+            return Spliterator.NONNULL;
+        }
     }
 }
