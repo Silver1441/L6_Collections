778fe6d53b09ef0e7fb5d4f759411e25b190db9e
Ivan <Kishkan91@gmail.com>
2019-07-19 23:48:47 +0400

    task2 refactoring complete

--- src/service/MatcherGroupIterator.java
+++ src/service/MatcherGroupIterator.java
@@ -1,4 +1,38 @@
 package service;
 
-public class MatcherGroupIterator {
+import java.util.Spliterator;
+import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatcherGroupIterator implements Spliterator<String> {
+    private final Matcher matcher;
+
+    public MatcherGroupIterator(Pattern p, String s) {
+        matcher = p.matcher(s);
+    }
+
+    @Override
+    public boolean tryAdvance(Consumer<? super String> action) {
+        if (!matcher.find()) {
+            return false;
+        }
+        action.accept(matcher.group());
+        return true;
+    }
+
+    @Override
+    public Spliterator<String> trySplit() {
+        return null;
+    }
+
+    @Override
+    public long estimateSize() {
+        return Long.MAX_VALUE;
+    }
+
+    @Override
+    public int characteristics() {
+        return Spliterator.NONNULL;
+    }
 }
--- src/task2/service/UniqueWordsCounter.java
+++ src/task2/service/UniqueWordsCounter.java
@@ -1,57 +1,19 @@
 package task2.service;
 
 
+import service.MatcherGroupIterator;
+
 import java.util.*;
-import java.util.function.Consumer;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-import static java.util.stream.Collectors.toSet;
 import static template.Patterns.*;
 
 public class UniqueWordsCounter {
     public static Set<String> countUniqueWords(String text) {
-        Pattern pattern = Pattern.compile(ONLY_WORDS);
-
-        Set<String> result = (StreamSupport.stream(new MatcherGroupIterator(pattern,text),false))
-                //.map( s -> s.toLowerCase() )
+        return (StreamSupport.stream(new MatcherGroupIterator(Pattern.compile(ONLY_WORDS),text),false))
+                .map( s -> s.toLowerCase() ) //making case insensitive
                 .collect(Collectors.toSet());
-        return result;
-
-    }
-
-    private static class MatcherGroupIterator implements Spliterator<String> {
-        private final Matcher matcher;
-
-        public MatcherGroupIterator(Pattern p, String s) {
-            matcher = p.matcher(s);
-        }
-
-        @Override
-        public boolean tryAdvance(Consumer<? super String> action) {
-            if (!matcher.find()) {
-                return false;
-            }
-            action.accept(matcher.group());
-            return true;
-        }
-
-        @Override
-        public Spliterator<String> trySplit() {
-            return null;
-        }
-
-        @Override
-        public long estimateSize() {
-            return Long.MAX_VALUE;
-        }
-
-        @Override
-        public int characteristics() {
-            return Spliterator.NONNULL;
-        }
     }
 }
