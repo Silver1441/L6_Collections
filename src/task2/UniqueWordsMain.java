d2d2efa565543b16afac8576348d749e8051ccdf
Ivan <Kishkan91@gmail.com>
2019-07-20 02:14:59 +0400

    minor refactoring

--- src/task2/UniqueWordsMain.java
+++ src/task2/UniqueWordsMain.java
@@ -1,13 +1,15 @@
 package task2;
 
+import task2.service.UniqueWordsCounter;
+
 import static template.TextHolder.*;
-import static task2.service.UniqueWordsCounter.countUniqueWords;
 import static task2.service.DemoOutput.showOutput;
 
 //Вывести все уникальные слова из текста. Рекомендуется использовать Set<String>.
 
 public class UniqueWordsMain {
     public static void main(String[] args) {
-        showOutput(countUniqueWords(text));
+        UniqueWordsCounter counter = new UniqueWordsCounter();
+        showOutput(counter.countUniqueWords(text));
     }
 }
--- src/task2/service/UniqueWordsCounter.java
+++ src/task2/service/UniqueWordsCounter.java
@@ -11,9 +11,9 @@ import java.util.stream.StreamSupport;
 import static template.Patterns.*;
 
 public class UniqueWordsCounter {
-    public static Set<String> countUniqueWords(String text) {
-        return (StreamSupport.stream(new MatcherGroupIterator(Pattern.compile(ONLY_WORDS),text),false))
-                .map( s -> s.toLowerCase() ) //making case insensitive
+    public Set<String> countUniqueWords(String text) {
+        return (StreamSupport.stream(new MatcherGroupIterator(Pattern.compile(ONLY_WORDS), text), false))
+                .map(s -> s.toLowerCase()) //making case insensitive
                 .collect(Collectors.toSet());
     }
 }
