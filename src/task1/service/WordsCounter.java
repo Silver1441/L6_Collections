1169738b555932c706e2ca7b62a85b513b7ee9ec
Ivan <Kishkan91@gmail.com>
2019-07-21 11:59:18 +0400

    task1 refactoring

--- src/task1/service/WordsCounter.java
+++ src/task1/service/WordsCounter.java
@@ -1,20 +1,15 @@
 package task1.service;
 
-import service.MatcherGroupIterator;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 import static template.Patterns.*;
 
 public class WordsCounter {
     public static Map<String, Long> countTheWords(String text) {
-
         Map<String, Long> words = new HashMap<>();
         Long counter = 1L;
 
@@ -31,19 +26,12 @@ public class WordsCounter {
             }
         }
 
+        //turning all words lowercase and remaking the set with less objects inside:
         return words.entrySet().stream()
                 .collect(Collectors.toMap(
                         word -> word.getKey().toLowerCase(),
                         word -> word.getValue(),
                         (oldValue, newValue) -> newValue
                 ));
-
-        //return words;
-/*
-        return (StreamSupport.stream(new MatcherGroupIterator(Pattern.compile(ONLY_WORDS), text), false))
-                .map(s -> s.toLowerCase()) //making case insensitive
-                .collect(Collectors.toMap(s1, 1));
-
- */
     }
 }
